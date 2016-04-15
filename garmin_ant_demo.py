c88c50f467b30943bd02817ecc3a37afbdf0c946
anthonypetersen <tonypetersen@hotmail.com>
2016-04-15 19:57:32 -0400

    ohmyohmy

--- garmin_ant_demo.py
+++ garmin_ant_demo.py
@@ -59,12 +59,11 @@ class HRM(event.EventCallback):
         self.channel.open()
 
 	def updateHR(newHR):
-		firebase.put('/HeartRate', 'HeartRate', newHR)
+		print firebase.put('/HeartRate', 'HeartRate', newHR)
 		gc.collect()
 		
     def process(self, msg):
         if isinstance(msg, message.ChannelBroadcastDataMessage):
-			print "{}".format(ord(msg.payload[-1]))
 			updateHR(ord(msg.payload[-1]))
 			
 SERIAL = '/dev/ttyUSB0'
